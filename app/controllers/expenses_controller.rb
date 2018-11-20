9c32345adb095cc0bcb9b473789652ab56ad7930
Angela Castagno <oesed16@gmail.com>
2018-11-20 14:50:33 -0500

    Reto Nro 11 Ruby On Rails

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,11 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+    @expenses = Expense.order("date DESC")
+    if params[:concept].present?
+      @expenses = @expenses.where("concept like '%#{params[:concept]}%'")
+    end
+    if params[:category_id].present?
+      @expenses = @expenses.where(category: params[:category_id])
+    end
   end
 end
